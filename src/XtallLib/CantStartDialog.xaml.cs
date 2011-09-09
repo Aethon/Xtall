db91a1a00a7e623db6a251bc34b7170ab04d964f
Brent McCullough <brent@brentm.info>
2011-09-09 01:49:26 -0500

    added failure dialog back into project; needs lots of styling work

---
+++ src/XtallLib/CantStartDialog.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XtallLib
+{
+    public partial class CantStartDialog
+    {
+        public CantStartDialog(CantStartViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/XtallLib/CantStartViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace XtallLib
+{
+    public class CantStartViewModel
+    {
+        public string Entry { get; set; }
+        public string Error { get; set; }
+        public string While { get; set; }
+        public string Log { get; set; }
+    }
+}
